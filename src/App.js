16694d21c2804f38ebf0c9ae2de81d60ab44da43
jason1110 <Jason.easterly11@gmail.com>
2021-02-19 11:54:28 -0700

    add name generator button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import { Component } from 'react'
 
-export default function App() {
-  return (
-    <div>
-      <h1>Welcome to the Stage!</h1>
-    </div>
-  )
+class App extends Component {
+  
+  state = {
+    name: "",
+  }
+
+  handleNameButton = () => {
+   this.setState({ name: "george" })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome to the Stage!</h1>
+        <button onClick={this.handleNameButton}>generate a name</button> 
+        <h2>{this.state.name}</h2>
+      </div>
+    )
+  } 
 }
+
+
+export default App;
